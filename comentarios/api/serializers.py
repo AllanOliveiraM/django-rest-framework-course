41191fa948c0709cb1651cef2cecec53603c93e9
Allan Oliveira Miraballes <allan.miraballes@nave.rs>
2021-05-28 00:34:49 -0300

    auto

---
+++ comentarios/api/serializers.py
@@ -0,0 +1,11 @@
+from rest_framework.serializers import ModelSerializer
+
+from comentarios.models import Comentario
+
+
+class ComentarioSerializer(ModelSerializer):
+    class Meta:
+        model = Comentario
+        fields = [
+            'id', 'usuario', 'comentario', 'data', 'aprovado'
+        ]
--- turismo/urls.py
+++ turismo/urls.py
@@ -6,12 +6,14 @@ from rest_framework import routers
 from core.api.viewsets import PontoTuristicoViewSet
 from atracoes.api.viewsets import AtracoesViewSet
 from enderecos.api.viewsets import EnderecoViewSet
+from comentarios.api.viewsets import ComentarioViewSet
 
 router = routers.DefaultRouter()
 
 router.register(r'pontoturistico', PontoTuristicoViewSet)
 router.register(r'atracoes', AtracoesViewSet)
 router.register(r'enderecos', EnderecoViewSet)
+router.register(r'comentarios', ComentarioViewSet)
 
 urlpatterns = [
     path('', include(router.urls)),
