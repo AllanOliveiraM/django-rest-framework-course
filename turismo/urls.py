7b275ba02a305c8df5b6e8bb3204ac6b1a167c10
Allan Oliveira Miraballes <allan.miraballes@nave.rs>
2021-10-08 17:42:46 -0300

    auto

--- turismo/urls.py
+++ turismo/urls.py
@@ -4,14 +4,14 @@ from django.urls import path, include
 from rest_framework import routers
 
 from core.api.viewsets import PontoTuristicoViewSet
-from atracoes.api.viewsets import AtracoesViewSet
+from atracoes.api.viewsets import AtracaoViewSet
 from enderecos.api.viewsets import EnderecoViewSet
 from comentarios.api.viewsets import ComentarioViewSet
 
 router = routers.DefaultRouter()
 
 router.register(r'pontoturistico', PontoTuristicoViewSet)
-router.register(r'atracoes', AtracoesViewSet)
+router.register(r'atracoes', AtracaoViewSet)
 router.register(r'enderecos', EnderecoViewSet)
 router.register(r'comentarios', ComentarioViewSet)
 
